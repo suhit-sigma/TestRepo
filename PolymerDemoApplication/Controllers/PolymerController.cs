11b7fff4c2b999800435f01ebbec986a432d5394
suhit.r <suhit.r@SISAHDLPT061>
2016-09-02 11:21:37 +0530

    Add project files.

---
+++ PolymerDemoApplication/Controllers/PolymerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PolymerDemoApplication.Controllers
+{
+    public class PolymerController : Controller
+    {
+        // GET: Polymer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PolymerDemoApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PolymerDemoApplication.Startup))]
+namespace PolymerDemoApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
